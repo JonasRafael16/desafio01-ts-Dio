3402d629a69508d3b358767fb58d4d33423a8a4c
Jonas Rafael de Lima <jonasrafael16@gmail.com>
2022-12-10 19:22:23 -0300

    feat: loan method

--- app.ts
+++ app.ts
@@ -3,7 +3,9 @@ import { PeopleAccount } from './class/PeopleAccount'
 
 const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
 console.log(peopleAccount)
-peopleAccount.deposit()
+peopleAccount.deposit(20)
 const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
+companyAccount.deposit(30.05)
+companyAccount.withdraw(10)
+companyAccount.getLoan(1000)
 console.log(companyAccount)
--- class/CompanyAccount.ts
+++ class/CompanyAccount.ts
@@ -2,11 +2,13 @@ import { DioAccount } from "./DioAccount"
 
 export class CompanyAccount extends DioAccount {
 
-  constructor(name: string, accountNumber: number){
+   constructor(name: string, accountNumber: number){
     super(name, accountNumber)
   }
 
-  getLoan = (): void => {
+  getLoan = (amountRequested: number): void => {
+    this.validateStatus()
+    this.setDepositBalance(amountRequested)
     console.log('Voce pegou um empréstimo')
   }
 }
